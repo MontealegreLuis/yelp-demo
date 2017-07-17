d2f8661b22c31137ffb523f3918943dd014584fd
Luis Montealegre <montealegreluis@gmail.com>
2017-07-16 19:46:10 -0500

    chore: Create Spring Boot application

---
+++ src/main/java/com/yelpfusion/YelpApplication.java
@@ -0,0 +1,12 @@
+package com.yelpfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpApplication.class, args);
+	}
+}
