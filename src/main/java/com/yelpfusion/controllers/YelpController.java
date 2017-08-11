c90e9056197cedb773fffefb3b2c54c40314d250
Luis Montealegre <montealegreluis@gmail.com>
2017-08-10 22:10:22 -0500

    feature: Add available categories to the search form

--- src/main/java/com/yelpfusion/controllers/YelpController.java
+++ src/main/java/com/yelpfusion/controllers/YelpController.java
@@ -6,13 +6,16 @@ package com.yelpfusion.controllers;
 import com.montealegreluis.yelpv3.Yelp;
 import com.montealegreluis.yelpv3.businesses.SearchResult;
 import com.montealegreluis.yelpv3.client.Credentials;
+import com.montealegreluis.yelpv3.jsonparser.SearchCategoryParser;
+import com.montealegreluis.yelpv3.search.SearchCategories;
 import com.montealegreluis.yelpv3.search.SearchCriteria;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Locale;
 
 @Controller
 public class YelpController {
@@ -26,7 +29,13 @@ public class YelpController {
     }
 
     @GetMapping("/")
-    public String showSearchForm() {
+    public String showSearchForm(Model viewModel) {
+        SearchCategories usCategories = SearchCategoryParser.all()
+            .main()
+            .availableAt(new Locale("en", "US"))
+        ;
+
+        viewModel.addAttribute("categories", usCategories);
         return "search";
     }
 
