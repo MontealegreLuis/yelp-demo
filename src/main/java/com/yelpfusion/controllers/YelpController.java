5d25d465996d5f95c6f0c853c51da6eef5decdde
Luis Montealegre <montealegreluis@gmail.com>
2017-08-19 14:24:34 -0500

    feature: Add map markers to search page

--- src/main/java/com/yelpfusion/controllers/YelpController.java
+++ src/main/java/com/yelpfusion/controllers/YelpController.java
@@ -3,6 +3,9 @@
  */
 package com.yelpfusion.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.montealegreluis.yelpv3.Yelp;
 import com.montealegreluis.yelpv3.businesses.Business;
 import com.montealegreluis.yelpv3.businesses.SearchResult;
@@ -21,10 +24,16 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Locale;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.ALL;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
+
 @Controller
 public class YelpController {
     private final Yelp yelp;
     private SearchCategories categories;
+    private final ObjectWriter writer;
 
     public YelpController(
         @Value("${yelp.api.client_id}") String yelpId,
@@ -35,6 +44,11 @@ public class YelpController {
             .main()
             .availableAt(new Locale("en", "US"))
         ;
+        ObjectMapper mapper = new ObjectMapper()
+            .setVisibility(ALL, NONE)
+            .setVisibility(FIELD, ANY)
+        ;
+        writer = mapper.writerWithDefaultPrettyPrinter();
     }
 
     @GetMapping("/")
@@ -44,22 +58,31 @@ public class YelpController {
     }
 
     @GetMapping("/search")
-    public String showSearchResults(@ModelAttribute SearchRequest request, Model viewModel) {
+    public String showSearchResults(
+        @ModelAttribute SearchRequest
+        request, Model viewModel
+    ) throws JsonProcessingException {
         SearchCriteria criteria = request.criteria();
         SearchResult result = yelp.search(criteria).searchResult();
         viewModel.addAttribute("categories", categories);
         viewModel.addAttribute("result", result);
+        viewModel.addAttribute("coordinates", writer.writeValueAsString(result.coordinates()));
+        viewModel.addAttribute("mapCenter", writer.writeValueAsString(result.region.center));
         viewModel.addAttribute("criteria", criteria);
         viewModel.addAttribute("pagination", criteria.pagination(result.total));
         return "search";
     }
 
     @GetMapping("/business/{businessId}")
-    public String viewBusiness(@PathVariable String businessId, Model viewModel) {
+    public String viewBusiness(
+        @PathVariable String businessId,
+        Model viewModel
+    ) throws JsonProcessingException {
         Business business = yelp.searchById(businessId).business();
         SearchCriteria criteria = SearchCriteria.byLocation(business.basicInformation.location.city);
 
         viewModel.addAttribute("business", business);
+        viewModel.addAttribute("mapCenter", writer.writeValueAsString(business.basicInformation.coordinates));
         viewModel.addAttribute("reviews", yelp.reviews(businessId).reviews());
         viewModel.addAttribute("criteria", criteria);
         viewModel.addAttribute("today", LocalDate.now().getDayOfWeek());
--- src/main/resources/static/map-business.js
+++ src/main/resources/static/map-business.js
@@ -1,14 +1,29 @@
 /*
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
-function Map(canvas, latitude, longitude) {
-    this.show = function () {
-        const business = {lat: latitude, lng: longitude};
-        const map = new google.maps.Map(canvas, {
-            zoom: 19,
-            center: business,
-            mapTypeId: google.maps.MapTypeId.SATELLITE
-        });
-        new google.maps.Marker({position: business, map: map});
+"use strict";
+
+function Map(canvas, center) {
+    const map = new google.maps.Map(canvas, {
+        zoom: 15,
+        center: {lat: center.latitude, lng: center.longitude}
+    });
+
+    this.changeTypeTo = function (newType) {
+        map.setMapTypeId(newType);
+    };
+
+    this.addMarkerAt = function (latitude, longitude) {
+        return new google.maps.Marker({position: {lat: latitude, lng: longitude}, map: map});
+    };
+
+    this.addMarkers = function (coordinates) {
+        var bounds = new google.maps.LatLngBounds();
+        var marker;
+        for (var i = 0; i < coordinates.length; i++) {
+            marker = this.addMarkerAt(coordinates[i].latitude, coordinates[i].longitude);
+            bounds.extend(marker.getPosition());
+        }
+        map.fitBounds(bounds);
     }
 }
